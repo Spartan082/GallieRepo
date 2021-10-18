ca34042b8576d0c9dffb81f38e974bc57b0d4af0
Sabrina110 <Sabrina110@verizon.net>
2021-10-18 16:44:04 -0400

    updated Profile look

--- galliefrontend/src/App.js
+++ galliefrontend/src/App.js
@@ -3,26 +3,69 @@ import Home from "./Home";
 import Register from "./Register";
 import Login from "./Login";
 import PremiumContent from "./PremiumContent";
+import ForgotPassword from "./ForgotPassword";
+import PublicRoute from "./routes/PublicRoute";
+import PrivateRoute from "./routes/PrivateRoute";
+import React, { useState, useEffect } from "react";
+import { getUser, getToken, setUserSession, resetUserSession } from "./service/AuthService";
+import axios from "axios";
+
+const verifyTokenAPIURL = 'https://pk3vioz5q4.execute-api.us-east-1.amazonaws.com/gallieprod3/verify';
 
 function App() {
+
+ const [isAuthenicating, setAuthenicating] = useState(true);
+
+  useEffect(() => {
+    const token = getToken();
+    if (token === 'undefined' || token === undefined || token === null || !token) {
+      return;
+    }
+
+    const requestConfig = {
+      headers: {
+        'x-api-key': '2tjbJfuJBWrpkvAUu5ax6lwdXKiYepG6yZhm9EJg'
+      }
+    }
+    const requestBody = {
+      user: getUser(),
+      token: token
+    }
+
+    axios.post(verifyTokenAPIURL, requestBody, requestConfig).then(response => {
+      setUserSession(response.data.user, response.data.token);
+      setAuthenicating(false);
+    }).catch(() => {
+      resetUserSession();
+      setAuthenicating(false);
+    })
+  }, []);
+
+  const token = getToken();
+  if (isAuthenicating && token) {
+    return <div className="content">Authenicating...</div>
+  }
+
   return (
     <div className="App">
-     <BrowserRouter>
-     <div className="header">
-       <NavLink exact activeClassName="active" to="/">{Home}</NavLink>
-       <NavLink activeClassName="active" to="/register">{Register}</NavLink>
-       <NavLink activeClassName="active" to="/login">{Login}</NavLink>
-       <NavLink activeClassName="active" to="/premium-content">{PremiumContent}</NavLink>
-     </div>
-     <div className="content">
-     <Switch>
-       <Route exact path="/" component={Home}/>
-       <Route path="/register" componet={Register}/>
-       <Route path="/login" componet={Login}/>
-       <Route path="/premium-content" componet={PremiumContent}/>
-     </Switch>
-     </div>
-     </BrowserRouter>
+      <BrowserRouter>
+      <div className="header">
+        <NavLink exact activeClassName="active" to="/">Home</NavLink>
+        <NavLink activeClassName="active" to="/register">Register</NavLink>
+        <NavLink activeClassName="active" to="/login">Login</NavLink>
+        <NavLink activeClassName="active" to="/premium-content">Premium Content</NavLink>
+        {/* <NavLink activeClassName="active" to="/forgot-password">Forgot Password</NavLink> */}
+      </div>
+      <div className="content">
+        <Switch>
+          <PublicRoute exact path="/" component={Home}/>
+          <PublicRoute path="/register" component={Register}/>
+          <PublicRoute path="/login" component={Login}/>
+          <PrivateRoute path="/premium-content" component={PremiumContent}/>
+          <PublicRoute path="/forgot-password" component={ForgotPassword}/>
+        </Switch>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
--- galliefrontend/src/Login.js
+++ galliefrontend/src/Login.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import { setUserSession } from './service/AuthService'
+import { NavLink} from "react-router-dom";
 
-const Login = () => {
-    return(
-        <div>
-        This is the login page!
-        </div>
-    )
 
+
+const loginAPIUrl = 'https://pk3vioz5q4.execute-api.us-east-1.amazonaws.com/gallieprod3/login';
+
+const Login = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (username.trim() === '' || password.trim() === '') {
+      setErrorMessage('Both username and password are required');
+      return;
+    }
+    //console.log('login button pressed')
+    setErrorMessage(null);
+    const requestConfig = {
+      headers: {
+        'x-api-key': '2tjbJfuJBWrpkvAUu5ax6lwdXKiYepG6yZhm9EJg'
+      }
+    }
+    const requestBody = {
+      username: username,
+      password: password
+    }
+
+    axios.post(loginAPIUrl, requestBody, requestConfig).then((response) => {
+      setUserSession(response.data.user, response.data.token);
+      props.history.push('/premium-content');
+    }).catch((error) => {
+      if (error.response.status === 401 || error.response.status === 403) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        setErrorMessage('sorry....the backend server is down. please try again later!!');
+      }
+    })
+  }
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <h1 class="form__title">Login</h1>
+        <div class="form__message form__message--error"></div>
+            <div class="form__input-group">
+                <input type="text"  class="form__input" autofocus placeholder="Username or email" value={username} onChange={event => setUsername(event.target.value)} /> <br/>
+                <div class="form__input-error-message"></div>
+            </div>
+            <div class="form__input-group">
+                <input type="password" class="form__input" autofocus placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} /> <br/>
+                <div class="form__input-error-message"></div>
+            </div>
+            <button class="form__button" type="submit">Continue</button>
+      </form>
+      {errorMessage && <p className="message">{errorMessage}</p>}
+      <p class="form__text">
+                <NavLink class="form__link" activeClassName="active" to="/forgot-password">Forgot your password?</NavLink>
+            </p> 
+            <p class="form__text">
+                <NavLink class="form__link" activeClassName="active" to="/register">Don't have a Profile? Create Profile</NavLink>
+            </p>
+    </div>
+  )
 }
 
 export default Login;
